89300b97a9b5f98fdcc99c1650cbe8328d27af78
FireWolfy89 <dmate899@gmail.com>
2023-12-19 17:14:38 +0100

    Token validation added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReserveController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+//Token middleware
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    Route::apiResource('/reservations', ReserveController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
 });
 
-Route::apiResource('/reservations', ReserveController::class);
